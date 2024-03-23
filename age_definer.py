bfd8cbe9328a2a478202daca187156baf1da9971
Abhijit <abhijitbhongle@gmail.com>
2024-03-23 13:02:54 +0000

    Implement changes for issue 1

--- age_definer.py
+++ age_definer.py
@@ -1,8 +1,13 @@
 # We need to write a code that gives out certain messages based on the age of the user.
+
 # The age will be an input to the the age asked.
+
 # Different age range will have different messages.
+
 # This has to to be done using if, elif and else statements.
+
 # Issue 1: Enhancing Age Range Messages
+
 age = int(input("Enter your age between 0 to 100?"))
 
 if age < 13:
